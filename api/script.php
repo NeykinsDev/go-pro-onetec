8e5b35306a9d7d11926b17b833e9d69baa6145f3
Marwan Bouserhir <marwanbouserhir@gmail.com>
2022-11-24 15:01:14 +0100

    update script.php

--- api/script.php
+++ api/script.php
@@ -6,9 +6,15 @@ exec("py ./python/shoot.py");
 // foreach ($files as $file) {
 //   echo $file;
 // }
-$items = glob("*.mp4", GLOB_NOSORT);
-array_multisort(array_map('filemtime', $items), SORT_NUMERIC, SORT_DESC, $items);
 
-// basename($items);
+// recuperer les fichiers mp4
+$myarray = glob("*.mp4");
+// afficher les videos par date de creation (plus recent en premier)
+function monTri($a, $b)
+{
+  return filemtime($b) <=> filemtime($a);
+};
+usort($myarray, "monTri");
 
-// header('Location: index.php');           
+
+// header('Location: index.php');
